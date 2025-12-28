006089810772025c67307e3d3cb8d0dcacf99c27
Abasiono Alexander <ikangenyinabasiono@gmail.com>
2025-12-28 13:32:14 +0100

    Enhance data visualization with scatter and trend plots
    Added scatter plot and trend line visualization for price vs convenience.

--- Regression.py
+++ Regression.py
@@ -4,6 +4,29 @@ import seaborn as sns
 # Import matplotlib.pyplot with alias plt
 import matplotlib.pyplot as plt
 
+#VISUALIZING TO UNDERSTAND THE DATA
+# Draw the scatter plot
+sns.scatterplot(x='n_convenience', y='price_twd_msq', data=taiwan_real_estate)
+
+# Show the plot
+plt.show()
+#----------------------------------------------------------
+
+# Draw the scatter plot
+sns.scatterplot(x="n_convenience",
+                y="price_twd_msq",
+                data=taiwan_real_estate)
+
+# Draw a trend line on the scatter plot of price_twd_msq vs. n_convenience
+sns.regplot(x='n_convenience',
+         y='price_twd_msq',
+         data=taiwan_real_estate,
+         ci=None,
+         scatter_kws={'alpha': 0.5})
+
+# Show the plot
+plt.show()
+
 # Draw the scatter plot
 sns.scatterplot(x='n_convenience', y='price_twd_msq', data=taiwan_real_estate)
 
