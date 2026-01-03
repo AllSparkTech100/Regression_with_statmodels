79275c690d06aadda4e029aa6673f2dd71494b56
Abasiono Alexander <ikangenyinabasiono@gmail.com>
2026-01-03 10:59:41 +0100

    Add comment for script purpose
    Added a comment to describe the purpose of the script.

---
+++ Extracting_model_elements.py
@@ -0,0 +1,31 @@
+#Extracting Model Elemenets and Predicting house prices
+
+# Print the model parameters of mdl_price_vs_conv
+print(mdl_price_vs_conv.params)
+
+# Print the fitted values of mdl_price_vs_conv
+print(mdl_price_vs_conv.fittedvalues)
+
+# Print the residuals of mdl_price_vs_conv
+print(mdl_price_vs_conv.resid)
+
+# Print a summary of mdl_price_vs_conv
+print(mdl_price_vs_conv.summary())
+
+
+# Calculating and predicting house prices
+# Get the coefficients of mdl_price_vs_conv
+coeffs = mdl_price_vs_conv.params
+
+# Get the intercept
+intercept = coeffs[0]
+
+# Get the slope
+slope = coeffs[1]
+
+# Manually calculate the predictions
+price_twd_msq = intercept + slope * explanatory_data
+print(price_twd_msq)
+
+# Compare to the results from .predict()
+print(price_twd_msq.assign(predictions_auto=mdl_price_vs_conv.predict(explanatory_data)))
