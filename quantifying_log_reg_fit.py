cbd18cfa9c07caaf4d352f9e55b1f24068183737
Abasiono Alexander <ikangenyinabasiono@gmail.com>
2026-01-20 14:43:01 +0100

    Create quantifying_log_reg_fit.py

---
+++ quantifying_log_reg_fit.py
@@ -0,0 +1,26 @@
+# Get the actual responses
+actual_response = churn['has_churned']
+
+# Get the predicted responses
+predicted_response = np.round(mdl_churn_vs_relationship.predict())
+
+# Create outcomes as a DataFrame of both Series
+outcomes = pd.DataFrame({'actual_response': actual_response,
+                         'predicted_response': predicted_response})
+
+# Print the outcomes
+print(outcomes.value_counts(sort = False))
+
+
+# Import mosaic from statsmodels.graphics.mosaicplot
+from statsmodels.graphics.mosaicplot import mosaic
+
+# Calculate the confusion matrix conf_matrix
+conf_matrix = mdl_churn_vs_relationship.pred_table()
+
+# Print it
+print(conf_matrix)
+
+# Draw a mosaic plot of conf_matrix
+mosaic(conf_matrix)
+plt.show()
